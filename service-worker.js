2ab7611375d70dc0b6b0a260d5295412fc3c87ba
camillepichon333-byte <camille.pichon333@gmail.com>
2026-09-07 15:16:49 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='mes-recettes-ig-bas-v5';
-const FILES_TO_CACHE=['./','./index.html','./style.css','./script.js','./manifest.json','./icon-192.png','./icon-512.png','./images/pommes.svg','./images/tarte.svg','./images/choco.svg','./images/legumes.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))})
+const CACHE="mes-recettes-ig-bas-magazine-v1";
+const ASSETS=["./","./index.html","./style.css","./script.js","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
